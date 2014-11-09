7a155e5de5c42720ef07af7108594df3a15a7846
Jesús Campón <jesus.campon@gmail.com>
2014-11-09 01:41:37 +0000

    Initial commit of the project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MongoDBExercises
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var connectionString = "mongodb://localhost";
+			MongoClient client = new MongoClient(connectionString); 
+			MongoServer server = client.GetServer();
+			MongoDatabase studentsDB = server.GetDatabase("students");
+			double iDocusCount = studentsDB.GetCollection("grades").Count();
+			Console.WriteLine("The total number of documents in the database is: " + iDocusCount.ToString());
+			
+
+		}
+	}
+}
